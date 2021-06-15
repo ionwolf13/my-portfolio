ed1f7c478f915d7df684689ee47a5aad2f788ae1
jesus_garcia <voli.scorp13@gmail.com>
2021-06-14 23:17:41 -0500

    added

--- my-portfolio/src/components/ProjectsComponent.js
+++ my-portfolio/src/components/ProjectsComponent.js
@@ -8,27 +8,27 @@ const ProjectsComponent = () => {
             <div className='projects-component' id='project-1'>
                     <h2>Upgrade</h2>
                     <p>description</p>
-                    <p>tools</p>
+                    <p>Ruby, JavaScript, React, PostgreSQL</p>
             </div>
             <div className='projects-component' id='project-2'>
                     <h2>Vanguard Academy</h2>
-                    <p>description</p>
+                    <p>Ruby, JavaScript, React, PostgreSQL</p>
                     <p>tools</p>
             </div>
             <div className='projects-component' id='project-3'>
                     <h2>Borgtek</h2>
                     <p>description</p>
-                    <p>tools</p>
+                    <p>Ruby, JavaScript, React, PostgreSQL</p>
             </div>
             <div className='projects-component' id='project-4'>
-                    <h2>Apartments</h2>
-                    <p>description</p>
+                    <h2>Apartments App</h2>
+                    <p>Ruby, Ruby on Rails, SQLite</p>
                     <p>tools</p>
             </div>
             <div className='projects-component' id='project-5'>
                     <h2>Video Game Review</h2>
                     <p>description</p>
-                    <p>tools</p>
+                    <p>Ruby and SQLite</p>
                     <p>is this gonna show up?</p>
             </div>
         </div>
